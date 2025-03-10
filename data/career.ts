1ae28447224cebf9d88d1119cffa84c9544782dc
JoWoon Kim <96nicekim@gmail.com>
2025-03-11 00:10:18 +0800

    feature: fixed typo

--- data/career.ts
+++ data/career.ts
@@ -4,7 +4,7 @@ export type CareerItem = {
   company: string
   range: string
   description: string[]
-  logo: string // Added logo field
+  logo: string
 }
 
 export const career: CareerItem[] = [
@@ -24,7 +24,7 @@ export const career: CareerItem[] = [
     id: "2",
     title: "Data Analyst",
     company: "LG Electronics Inc, Vietnam",
-    range: "OCT 2021 - OCT 2022",
+    range: "OCT 2021 - SEP 2022",
     description: [
       "- Analyzed data to minimize wasted materials, resulting in total savings of 21 million USD over one year.",
       "- Took the initiative to design a database for collecting and integratingaround 45,000 daily records from multiple corporate systems, and pre-processed the data to ensure accurate integration.",
@@ -36,7 +36,7 @@ export const career: CareerItem[] = [
     id: "3",
     title: "Project manager",
     company: "LG Electronics Inc, Vietnam",
-    range: "AUG 2019 - OCT 2022",
+    range: "AUG 2019 - SEP 2022",
     description:[
       "- Ensured timely readiness for mass production of IVI (In-VehicleInfotainment) systems by overseeing key metrics such as defect ratesand production rates, achieving a 73% defect reduction.",
       "- Collaborated with other teams to coordinate production trial runs andmanage project documentation, to guarantee smooth project execution.",
